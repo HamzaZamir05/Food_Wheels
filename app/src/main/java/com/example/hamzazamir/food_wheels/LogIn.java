def6e6f68bdab0fe0af023e40c3a7d1ed22dbfe1
HamzaZamir05 <hamzazamir85@gmail.com>
2017-12-15 10:03:59 +0500

    onDouble back
    on double back pressed in login class

--- app/src/main/java/com/example/hamzazamir/food_wheels/LogIn.java
+++ app/src/main/java/com/example/hamzazamir/food_wheels/LogIn.java
@@ -2,6 +2,7 @@ package com.example.hamzazamir.food_wheels;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -97,11 +98,26 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener {
 
     }
 
-    public void onBackPressed() {
-    Toast.makeText(getApplicationContext(), "Press Home Button for exiting" , Toast.LENGTH_SHORT).show();
-       startActivity(new Intent(this, LogIn.class));
-    }
+    boolean doubleBackToExitPressedOnce = false;
 
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 
     @Override
     public void onClick(View view) {
